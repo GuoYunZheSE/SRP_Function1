f5c372a3ab5c5d13303e73368762ac6b64114f7e
GuoYunZheSE <guoyunzhe.SE@gmail.com>
2017-10-10 00:02:09 +0800

    10.10 in C10

---
+++ SRP_Function1/Identify.py
@@ -0,0 +1,9 @@
+import numpy
+class Identify(object):
+    """description of class"""
+    def __init__(self,Lable,Begin,End,Hide,ROI):
+        self.Lable=Lable
+        self.Begin=Begin
+        self.End=End
+        self.Hide=Hide
+        self.ROI=ROI
--- SRP_Function1/SRP_Function1.py
+++ SRP_Function1/SRP_Function1.py
@@ -1,9 +1,19 @@
 import  os
+import numpy
+import re
+from Identify import Identify
+Instance=[]
 path=r'c:\users\user\documents\visual studio 2017\Projects\SRP_Function1\SRP_Function1\DataSet'
 os.chdir(path)
 print('Please input the frame of the image: ')
 ID=input()
 for filename in os.listdir():
+    Instance.append(filename)
     file=open(filename,'r')
-    #process each txt
+    for eachline in file.readlines():
+        if eachline[:4]=='lbl=':
+            Temp_Data=re.split('lbl\=| str\=| end\=| hide\=',eachline)
+        if eachline[:5]=='pos =':
 
+        Instance[Instance.index(filename)]
+    #process each txt
