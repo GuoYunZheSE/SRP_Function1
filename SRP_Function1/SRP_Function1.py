52c31de1ab75efaf3fd08b767c249d9db94b5561
GuoYunZheSE <guoyunzhe.SE@gmail.com>
2017-10-09 21:30:54 +0800

    10.9 in B8

--- SRP_Function1/SRP_Function1.py
+++ SRP_Function1/SRP_Function1.py
@@ -1,1 +1,9 @@
+import  os
+path=r'c:\users\user\documents\visual studio 2017\Projects\SRP_Function1\SRP_Function1\DataSet'
+os.chdir(path)
+print('Please input the frame of the image: ')
+ID=input()
+for filename in os.listdir():
+    file=open(filename,'r')
+    #process each txt
 
